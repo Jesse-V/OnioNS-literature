15997c37bdc0c89d0b6be126918af5e509b7530e
Jesse Victors <jvictors@jessevictors.com>
2015-05-07 11:05:12 -0800

    initial conversion to ACM, cleanup

--- clean.sh
+++ clean.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 rm -f thesis.aux thesis.bbl thesis.blg thesis.lof thesis.log thesis.out thesis.synctex.gz thesis.toc
-rm -f doc/*.aux
-rm -f doc/*.log
+rm -f conference/acm-ccs.aux conference/acm-ccs.bbl conference/acm-ccs.blg conference/acm-ccs.lof conference/acm-ccs.log conference/acm-ccs.out conference/acm-ccs.synctex.gz conference/acm-ccs.toc
 
-rm -f *.tex.bak
-rm -f doc/*.tex.bak
+rm -f doc/*.aux conference/*.aux
+rm -f doc/*.log conference/*.log
+
+rm -f *.tex.bak conference/*.tex.bak
+rm -f doc/*.tex.bak conference/*.tex.bak
